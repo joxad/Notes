4bcc860e4ba0e119678a7c4d675e5a5933d3fa31
Jocelyn David <external.z17jdavi>
2016-11-27 18:42:42 +0100

    preaprae auth

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import {ViewController} from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+import { Account } from '../../model/account';
+import { Validators, FormGroup, FormControl} from '@angular/forms';
+
 @Component({
   selector: 'page-account',
   templateUrl: 'account.html'
-}) export class AccountPage implements OnInit {
+})
+export class AccountPage implements OnInit {
 
+  isCreate : boolean;
+  account : Account;
   constructor(private viewCtrl: ViewController) {
-
+    this.isCreate = false;
+    this.account =  new Account();
   }
 
   ngOnInit(): void {
 
+  }
+
+
+  connect(): void {
+    //TODO call auth,
+    // if email exist : try to connect
+      // if pwd wrong show error password
+    // if email !exist, show is create
+    // update button to createaccount
+    this.isCreate = true;
+  }
+
+  createAccount() : void {
   }
 
   dismiss(): void {
